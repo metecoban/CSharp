68e3dc31decc402704fee564a7c97ef160165ad3
metecoban <64808380+metecoban@users.noreply.github.com>
2020-08-08 20:24:35 +0300

    Add files via upload

---
+++ Static Members for git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Static_members_for_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* We dont create any object from class*/
+            Console.WriteLine("Our Password: "+Start.Password); // with variable
+            Start.Text(); // with parameterless
+            Start.Text("Mete"); // with parameter
+
+        }
+    }
+}
---
+++ Static Members for git/Start.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_members_for_git
+{
+    class Start
+    {
+        public static int Password=1515; // Static memmer
+
+        public static void Text() // Static memmer
+        {
+            Console.WriteLine("Welcome to program!");
+        }
+        public static void Text(string name) // Static memmer
+        {
+            Console.WriteLine("Hi "+name+", Welcome to program!");
+        }
+
+    }
+}
