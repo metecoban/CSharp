6d9ad8549e15860f8b27ab7a0f9ec178193a0812
metecoban <64808380+metecoban@users.noreply.github.com>
2020-08-08 14:21:16 +0300

    Add files via upload

---
+++ encapsulation for git/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace encapsulation_for_git
+{
+    class Person
+    {
+        private string name; // We can't reach directly.
+        private int age; // We can't reach directly.
+
+        public string ReachName // indirect access about name
+        {
+            get
+            {
+                return name; 
+            }
+            set
+            {
+                name = value; 
+            }
+        }
+        public int ReachAge // indirect access about age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
---
+++ encapsulation for git/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace encapsulation_for_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person one = new Person(); // create a object
+            /*
+            one.name="Mete"; We can't reach like this, because we use private
+            */
+            one.ReachName = "Mete"; // You dont need to specify for setting 
+            one.ReachAge = 20;
+
+            Console.WriteLine(one.ReachName); // You dont need to specify for getting
+            Console.WriteLine(one.ReachAge);
+        }
+    }
+}
