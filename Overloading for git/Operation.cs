4d6cd622ba28987b91bc80a59b1bba3321390a08
metecoban <64808380+metecoban@users.noreply.github.com>
2020-08-08 19:50:46 +0300

    Add files via upload

---
+++ Overloading for git/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overloading_for_git
+{
+    class Operation
+    {
+        public int count;
+        public int count2;
+        public float count3;
+        public float Control(int first, float second) // Overloading  => The variable type returned is important like float.
+        {
+            return first + second;
+        }
+
+        public int Control(int first,int second) // Overloading  => The variable type returned is important like int.
+        {
+            return first + second;
+        }
+
+        public void Control() // Overloading  
+        {
+            // empty
+        }
+    }
+}
